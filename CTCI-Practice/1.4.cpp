9869f25fd5cd2f0c3459e8dce42e06fa09da02e5
jpark2383 <jpark2383@gmail.com>
2016-08-23 14:03:54 -0500

    replaceSpaces

--- CTCI-Practice/1.4.cpp
+++ CTCI-Practice/1.4.cpp
@@ -3,3 +3,41 @@
 #include <string.h>
 
 using namespace std;
+
+void replaceSpaces (char *s, int len)
+{
+	int numSpaces = 0;
+	int i = 0;
+	int j = 0;
+	for(i = 0; i < len; i++)
+	{
+		if(s[i] == ' ')
+		{
+			numSpaces++;
+		}
+	}
+	int extended = len + 2 * numSpaces;
+	i = extended - 1;
+	for(j = len - 1; j >= 0; j--)
+	{
+		if (s[j] != ' ')
+		{
+			s[i--] = s[j];
+		}
+		else
+		{
+			s[i--] = '0';
+			s[i--] = '2';
+			s[i--] = '%';
+		}
+	}
+}
+
+int main()
+{
+	char str[] = "I don't know what this is";
+	cout << "Actual String : " << str << endl;
+	replaceSpaces(str, 25);
+	cout << "String with spaces replaced with %20 : " << str << endl;
+	return 0;
+}
